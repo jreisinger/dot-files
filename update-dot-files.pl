add09cb980f794b52059cacaba35c93688dad883
Jozef Reisinger <jozef.reisinger@gmail.com>
2012-04-28 23:46:06 +0200

    add bashrc for cygwin

--- update-dot-files.pl
+++ update-dot-files.pl
@@ -9,33 +9,29 @@ use File::Spec;
 use File::Copy;
 use 5.010;
 
-# clone git repo into a temporary directory
-my $tmpdir = File::Temp->newdir( DIR => '/tmp' );
-chdir $tmpdir;
-system 'git clone git@github.com:jreisinger/dot-files.git';
+sub get_dot_files {
+    # clone git repo into a temporary directory
+    my $tmpdir = File::Temp->newdir( DIR => '/tmp' );
+    chdir $tmpdir;
+    system 'git clone git@github.com:jreisinger/dot-files.git';
 
-# get the dot files
-chdir 'dot-files';
-my @dot_files;
-for my $file ( glob "*" ) {
-    next if $file eq 'README' or $file eq 'update-dot-files.pl';
-    push @dot_files, $file;
-}
-
-print "@dot_files";
-
-my $os = $^O;
-given ( $os ) {
-    when( $os eq 'cygwin' ) {
-        @dot_files = grep $_ =~ /^_|^cygwin/, @dot_files;
-        print @dot_files;
-        continue;
+    # get the dot files
+    chdir 'dot-files';
+    my @dot_files;
+    for my $file ( glob "*" ) {
+        next if $file eq 'README';
+        next if $file eq 'update-dot-files.pl';
+        push @dot_files, $file;
     }
+    # cd to homedir to allow removal of the temporary directory
+    chdir;
+
+    return @dot_files;
 }
 
-
-# replace dot files in home dir with the repo version
 sub copy_dot_files {
+    # replace dot files in home dir with the repo version
+
     my @dot_files = @_;
 
     for my $file ( @dot_files ) {
@@ -45,5 +41,14 @@ sub copy_dot_files {
     }
 }
 
-# cd to homedir to allow removal of the temporary directory
-chdir;
+# MAIN
+my @dot_files = get_dot_files;
+
+my $os = $^O;
+given ( $os ) {
+    when( $os eq 'cygwin' ) {
+        @dot_files = grep $_ =~ /^_|^cygwin/, @dot_files;
+        print "@dot_files";
+        continue;
+    }
+}
